5d58f731eccf28ce65a8e98a2a77db3da40e7be7
marrcamino <rickmar052702camino@gmail.com>
2026-07-01 17:15:21 +0800

    refactor: add properties on database table `Wed Jul 1, 2026`

--- src/routes/app.d.ts
+++ src/routes/app.d.ts
@@ -53,6 +53,15 @@ declare global {
     position: string
     /** ex. 1999-01-01 06:30:55 */
     created_at: string;
+    signatory_pk: number;
+    full_name: string;
+    position_title: string;
+    effective_from: string;
+    effective_until: string | null
+    /** ex. 1999-01-01 06:30:55 */
+    created_at: string
+    /** ex. 1999-01-01 06:30:55 */
+    updated_at: string
   }
 
   interface PassSlip {
